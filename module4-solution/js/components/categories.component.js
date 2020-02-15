3869e9218bfdbd6ca2dbd9e8c4bbc2a0407bc029
AnkurMittal <reply.as.soon@gmail.com>
2020-02-15 19:05:03 +0530

    Initial Commit

---
+++ module4-solution/js/components/categories.component.js
@@ -0,0 +1,14 @@
+(function() {
+  "use strict";
+
+  angular.module("MenuApp").component("categories", {
+    templateUrl: "js/components/categories.template.html",
+    controller: CategoriesComponentController,
+    bindings: {
+      items: "<"
+    }
+  });
+
+  CategoriesComponentController.$inject = [];
+  function CategoriesComponentController() {}
+})();
