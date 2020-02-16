173e24856ee2ac4403cfd94b6963f058cb366d98
AnkurMittal <reply.as.soon@gmail.com>
2020-02-16 11:31:55 +0530

    Updated

--- module5-solution/src/public/public.routes.js
+++ module5-solution/src/public/public.routes.js
@@ -47,25 +47,37 @@
           ]
         }
       })
-      .state("public.sign-up", {
-        url: "/sign-up",
-        templateUrl: "src/public/sign-up/sign-up.html",
-        controller: "SignUpController",
-        controllerAs: "signUpCtrl"
-      })
       .state("public.my-info", {
         url: "/my-info",
         templateUrl: "src/public/my-info/my-info.html",
         controller: "MyInfoController",
         controllerAs: "myInfoCtrl",
         resolve: {
-          user: [
-            "UsersService",
-            function(UsersService) {
-              return UsersService.getUser();
+          myInfo: [
+            "SignUpStorage",
+            function(SignUpStorage) {
+              return SignUpStorage.get();
+            }
+          ],
+          favDish: [
+            "SignUpStorage",
+            "MenuService",
+            function(SignUpStorage, MenuService) {
+              var signUpInfo = SignUpStorage.get();
+              if (signUpInfo) {
+                return MenuService.getMenuItem(signUpInfo.favDish);
+              }
+
+              return null;
             }
           ]
         }
+      })
+      .state("public.sign-up", {
+        url: "/sign-up",
+        templateUrl: "src/public/sign-up/sign-up.html",
+        controller: "SignUpController",
+        controllerAs: "signUpCtrl"
       });
   }
 })();
